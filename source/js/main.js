e817ba95180990fba310eab8f2f7a80e37a20d1e
Timur <t.tokumov@gmail.com>
2022-04-24 19:51:42 +0600

    feat: add promotion block

--- source/js/main.js
+++ source/js/main.js
@@ -83,6 +83,8 @@ function activateTab(quantity = 12, coach = 5000, day = 1700, fullDay = 2700) {
 
 // //////////////////////////////////////////////////////////////////////T A B S//////////////////////////////////////////////////////////////////////////////////////////////////
 
+// //////////////////////////////////////////////////////////////////////P A S S E S    C A R D S     S I Z E//////////////////////////////////////////////////////////////////////////////////////////////////
+
 const passes = document.querySelector('.passes');
 const cardsBlock = document.querySelector('.passes__cards-block');
 const mediaPassesDesktop = window.matchMedia('(min-width: 1024px)');
@@ -113,3 +115,123 @@ function changePassesBlock() {
 }
 
 changePassesBlock();
+
+// //////////////////////////////////////////////////////////////////////P A S S E S    C A R D S     S I Z E//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// //////////////////////////////////////////////////////////////////////C O A C H E S     S L I D E R//////////////////////////////////////////////////////////////////////////////////////////////////
+const coaches = document.querySelector('.coaches');
+const coachesSlider = document.querySelector('.coaches__slider');
+const coachesLine = document.querySelector('.coaches__line');
+const coachesImages = document.querySelectorAll('.coaches__person-card img');
+const coachesLeftArrow = document.querySelector('#coaches-left-arrow');
+const coachesRightArrow = document.querySelector('#coaches-right-arrow');
+const coachesContainer = document.querySelector('.coaches__container');
+
+let leftArrow = 0;
+let rightArrow = 0;
+let sliderWidth;
+let leftArrowClick = false;
+let rightArrowClick = false;
+
+window.addEventListener('resize', setCoachesSlider);
+
+function setCoachesSlider() {
+  setTimeout(() => {
+    coachesLine.style.transform = 'translate(0)';
+  }, 500);
+}
+
+coachesLeftArrow.addEventListener('click', () => {
+  leftArrowClick = true;
+  setTimeout(() => {
+    rightArrowClick = false;
+  }, 50);
+  leftArrow = leftArrow - 1200;
+
+  function getTranslateX() {
+    const style = window.getComputedStyle(coachesLine);
+    const matrix = new DOMMatrixReadOnly(style.transform).m41;
+    return matrix;
+  }
+
+  if (getTranslateX() >= 0) {
+    leftArrow = 2400;
+  } else if (rightArrowClick === true) {
+    leftArrow = 0;
+  }
+
+  coachesLine.style.transform = `translateX(${-(leftArrow)}px)`;
+});
+
+coachesRightArrow.addEventListener('click', () => {
+  rightArrowClick = true;
+
+  setTimeout(() => {
+    leftArrowClick = false;
+  }, 50);
+
+  rightArrow = rightArrow - 1200;
+
+  function getTranslateX() {
+    const style = window.getComputedStyle(coachesLine);
+    const matrix = new DOMMatrixReadOnly(style.transform).m41;
+    return matrix;
+  }
+
+  if (getTranslateX() <= -2400) {
+    rightArrow = 0;
+  } else if (leftArrowClick === true) {
+    rightArrow = 0;
+  }
+
+  coachesLine.style.transform = `translateX(${(rightArrow)}px)`;
+});
+
+window.addEventListener('resize', () => {
+  if (window.matchMedia('(min-width: 1301px)').matches) {
+    coachesSlider.style.width = '1162px';
+    coachesImages.forEach((item) => {
+      item.style.width = '260px';
+      item.style.height = '294px';
+    });
+  } else if (window.matchMedia('(min-width: 1024px) and (max-width: 1300px)').matches) {
+    sliderWidth = coaches.offsetWidth;
+    coachesContainer.style.maxWidth = `${width / 222}`;
+    coachesSlider.style.maxWidth = '1162px';
+    coachesSlider.style.width = `${width / 1.1}px`;
+    coachesImages.forEach((item) => {
+      item.style.width = `${width / 5}px`;
+      item.style.height = 'auto';
+    });
+  } else if (window.matchMedia('(min-width: 768px) and (max-width: 1023px)').matches) {
+    coachesSlider.style.maxWidth = `${width / 1.7}px`;
+    coachesImages.forEach((item) => {
+      item.style.width = `${width / 3.56}px`;
+      item.style.height = 'auto';
+    });
+  }
+});
+
+const advantagesLeftBlock = document.querySelector('.advantages__left-block');
+const advantagesRightBlock = document.querySelector('.advantages__right-block');
+const advantageArea = document.querySelector('.advantages__area');
+const advantageComfort = document.querySelector('.advantages__comfort');
+const advantageParking = document.querySelector('.advantages__parking');
+// const advantageExperience = document.querySelector('.advantages__experience');
+
+
+function replaceAdvantageBlocks() {
+  if (window.matchMedia('(min-width: 1024px)').matches) {
+    advantagesLeftBlock.appendChild(advantageComfort);
+    advantagesRightBlock.prepend(advantageParking);
+  } else if (window.matchMedia('(max-width: 1023px)').matches) {
+    advantagesLeftBlock.appendChild(advantageParking);
+    advantagesRightBlock.prepend(advantageComfort);
+  }
+}
+
+replaceAdvantageBlocks();
+
+window.addEventListener('resize', () => {
+  replaceAdvantageBlocks();
+});
